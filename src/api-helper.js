c718ac7cd41a16ff6e461bbd501343a45d7a0978
Madeline Rombes <madelinerombes@gmail.com>
2019-07-25 14:18:56 -0400

    first axios call

--- src/api-helper.js
+++ src/api-helper.js
@@ -1,3 +1,8 @@
 import axios from 'axios'
 const appId = "399K7K-5Y3YVE5V4X"
+const BASE_URL = "http://api.wolframalpha.com/v2/"
 
+export default fetchData = async () => {
+  const data = await axios.get(`${BASE_URL}/query?input=pi/&appid=${appId}`)
+  return data
+}
